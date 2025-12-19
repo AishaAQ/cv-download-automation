e49d3e27a4296617044c5e1c832e775f5f396b8b
Aisha Abdul Quadir <aisha.abdulquadirzaifullah@gmail.com>
2025-12-20 01:35:38 +0300

    setting env

---
+++ google-docs-automation.py
@@ -0,0 +1,7 @@
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseDownload
+from google.oauth2 import service_account
+import io
+from datetime import datetime
+
+
