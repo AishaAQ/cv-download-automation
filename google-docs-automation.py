a2061ed5a9f7ad6b66227ffd56b7609510e8addc
Aisha Abdul Quadir <aisha.abdulquadirzaifullah@gmail.com>
2025-12-20 03:00:49 +0300

    folder names parsed from file names

--- google-docs-automation.py
+++ google-docs-automation.py
@@ -4,6 +4,31 @@ from google.oauth2 import service_account
 import io
 from datetime import datetime
 
+def download(file_id,file_name,file_type):
+
+    if file_type == 'pdf':
+        mime_type = 'application/pdf'
+    else:
+        mime_type = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+    
+    name_list = file_name.split('CV')
+    folder_name = name_list[1].replace('_', ' ')[1:]
+    
+    request = service.files().export_media(
+        fileId=file_id,
+        mimeType=mime_type
+    )
+
+    fh = io.FileIO('DownloadedDoc.' + file_type, 'wb')
+    downloader = MediaIoBaseDownload(fh, request)
+
+    done = False
+    while not done:
+        status, done = downloader.next_chunk()
+        print(f"Download {int(status.progress() * 100)}% complete")
+
+    print("Download finished!")
+
 SERVICE_ACCOUNT_FILE = 'service-account.json'
 
 credentials = service_account.Credentials.from_service_account_file(
@@ -22,18 +47,9 @@ files = results.get('files', [])
 for f in files:
 
     file_id = f['id']
-
-    request = service.files().get_media(fileId=file_id)
+    file_name = f['name']
 
     print(f"{f['name']} ({f['id']})")
 
-    fh = io.FileIO('DownloadedDoc.docx', 'wb')
-    downloader = MediaIoBaseDownload(fh, request)
-
-    done = False
-    while not done:
-        status, done = downloader.next_chunk()
-        print(f"Download {int(status.progress() * 100)}% complete")
-
-    print("Download finished!")
-
+    download(file_id,file_name,'pdf')
+    download(file_id,file_name,'docx')
